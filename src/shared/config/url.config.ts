9214fd68a573bd8c94f3b5780679d64b106d067a
Gosudarev Danila <danidagosudarev@gmail.com>
2025-09-27 17:43:27 +0300

    add config api url and app url

--- src/shared/config/url.config.ts
+++ src/shared/config/url.config.ts
@@ -3,6 +3,7 @@ export const APP_URL = process.env.APP_URL as string;
 export const PUBLIC_URL = {
 	root: (url = "") => `${url ? url : ""}`,
 	home: () => PUBLIC_URL.root("/"),
+
 	auth: () => PUBLIC_URL.root("/auth"),
 	explorer: (query = "") => PUBLIC_URL.root(`/explorer${query}`),
 	product: (id = "") => PUBLIC_URL.root(`/product/${id}`),
@@ -12,7 +13,31 @@ export const PUBLIC_URL = {
 export const DASHBOARD_URL = {
 	root: (url = "") => `/dashboard${url ? url : ""}`,
 	home: () => DASHBOARD_URL.root("/"),
+
 	favorites: () => DASHBOARD_URL.root("/favorites"),
 };
 
-export const STORE_URL = {};
+export const STORE_URL = {
+	root: (url = "") => `/store${url ? url : ""}`,
+	home: (storeId = "") => STORE_URL.root(`/${storeId}`),
+
+	products: (storeId = "") => STORE_URL.root(`/${storeId}/products`),
+	productCreate: (storeId = "") =>
+		STORE_URL.root(`/${storeId}/products/create`),
+	productEdit: (storeId = "", productId = "") =>
+		STORE_URL.root(`/${storeId}/products/${productId}`),
+
+	categories: (storeId = "") => STORE_URL.root(`/${storeId}/categories`),
+	categoryCreate: (storeId = "") =>
+		STORE_URL.root(`/${storeId}/categories/create`),
+	categoryEdit: (storeId = "", categoryId = "") =>
+		STORE_URL.root(`/${storeId}/categories/${categoryId}`),
+
+	colors: (storeId = "") => STORE_URL.root(`/${storeId}/colors`),
+	colorCreate: (storeId = "") => STORE_URL.root(`/${storeId}/colors/create`),
+	colorEdit: (storeId = "", colorId = "") =>
+		STORE_URL.root(`/${storeId}/colors/${colorId}`),
+
+	reviews: (storeId = "") => STORE_URL.root(`/${storeId}/reviews`),
+	settings: (storeId = "") => STORE_URL.root(`/${storeId}/settings`),
+};
